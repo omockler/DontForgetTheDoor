22db986e9428a98351b8bacc7e0414c51626e40c
Owen Mockler <omockler@gmail.com>
2013-10-14 00:39:55 +0000

    Switching away from time based auto close.
    There was an issue with the open door sensor flipping twice when opening and causing the auto close logic to fail

--- pi/job_watcher.rb
+++ pi/job_watcher.rb
@@ -10,7 +10,7 @@ api_client = ApiClient.new
 loop do
   sleep 60
   
-  api_client.send_status(sensor_pin.on?)
+  api_client.send_status(sensor_pin.read == 1)
 
   job = api_client.get_job
 
@@ -19,13 +19,13 @@ loop do
   # Dont process the job if the door is already in the desired state.
   # TODO: Close put job with fail
 
-  next if job["type"] == "open" and sensor_pin.on?
-  next if job["type"] == "closed" and sensor_pin.off?
+  next if job["type"] == "open" and sensor_pin.value == 1
+  next if job["type"] == "closed" and sensor_pin.vslue == 0
 
   motor_pin.on
   sleep 5
   motor_pin.off
 
   # ensure door status
-  api_client.finish_job job["id"], true, sensor_pin.off?  
+  api_client.finish_job job["id"], true, sensor_pin.read == 1
 end
--- pi/pin_watcher.rb
+++ pi/pin_watcher.rb
@@ -10,22 +10,20 @@ sensor_pin = PiPiper::Pin.new pin: 10, direction: :in, invert: true
 motor_pin = PiPiper::Pin.new pin: 7, direction: :out, pull: :up
 override_pin = PiPiper::Pin.new pin: 0, direction: :in, invert: true
 
-after :pin => 10, :goes => :low do
-  return if override_pin.on?
+after :pin => 10, :goes => :high do
+  return if override_pin.read == 1
   puts "Door open. Waiting..."
-  begin
-    Timeout.timeout(60) {
-      sensor_pin.wait_for_change
-      puts "Door closed in time."
-    }
-   rescue
-     puts "Auto closing"
-     motor_pin.on
-     sleep 5
-     motor_pin.off
-     api_client.auto_close
-     api_client.send_status(sensor_pin.on?)
-   end
+  sleep 60 * 15
+  if sensor_pin.read == 1
+    puts "Auto Closing"
+    motor_pin.on
+    sleep 5
+    motor_pin.off
+    api_client.auto_close
+    api_client.send_status(sensor_pin.read == 1)
+  else
+    puts "Door closed in time"
+  end
 end
 
 PiPiper.wait
