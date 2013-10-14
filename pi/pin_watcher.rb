a5f47f27ceb053d79e92bb3fa98883f4e2527c00
Owen Mockler <omockler@gmail.com>
2013-10-14 00:52:28 +0000

    Fixing override to work even if the door has already been opened

--- pi/pin_watcher.rb
+++ pi/pin_watcher.rb
@@ -11,16 +11,17 @@ motor_pin = PiPiper::Pin.new pin: 7, direction: :out, pull: :up
 override_pin = PiPiper::Pin.new pin: 0, direction: :in, invert: true
 
 after :pin => 10, :goes => :high do
-  return if override_pin.read == 1
   puts "Door open. Waiting..."
   sleep 60 * 15
-  if sensor_pin.read == 1
+  if sensor_pin.read == 1 and override_pin.read == 0
     puts "Auto Closing"
     motor_pin.on
     sleep 5
     motor_pin.off
     api_client.auto_close
     api_client.send_status(sensor_pin.read == 1)
+  elsif override_pin.read == 1
+    puts "Override activated."
   else
     puts "Door closed in time"
   end
